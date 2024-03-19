6509014281b6603d6a495f1833717b6f0cb06f62
ajr3021 <54864764+ajr3021@users.noreply.github.com>
2024-03-19 11:03:24 -0400

    Example route

--- backend/server.py
+++ backend/server.py
@@ -9,7 +9,51 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route("/message")
 @cross_origin(origins="*")
 def index():
-    return {"message": "hello"}
+    import psycopg2
+    from sshtunnel import SSHTunnelForwarder
+
+    username = ""
+    password = ""
+    dbName = "p320_10"
+
+    try:
+        with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                                ssh_username=username,
+                                ssh_password=password,
+                                remote_bind_address=('127.0.0.1', 5432)) as server:
+            server.start()
+            print("SSH tunnel established")
+            params = {
+                'database': dbName,
+                'user': username,
+                'password': password,
+                'host': 'localhost',
+                'port': server.local_bind_port
+            }
+
+
+            conn = psycopg2.connect(**params)
+            curs = conn.cursor()
+            print("Database connection established")
+
+            sql = "SELECT * FROM collection;"
+
+            curs.execute(sql)
+            result = curs.fetchall() 
+
+            print(result)
+
+            conn.commit()
+
+            conn.close()
+
+            return {"message": str(result)}
+    except Exception as e:
+        print("Connection failed")
+        print(e)
+        return {"message": "Exception"}
+        
+    
 
 
 if __name__ == "__main__":
